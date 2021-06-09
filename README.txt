f5d3937db7ba0103843e9e0410ea365fffd5cf6d
svr1106 <svr1106@gmail.com>
2021-06-09 15:12:38 +0530

    angular+webapi

---
+++ README.txt
@@ -0,0 +1,4 @@
+Here i have Angular as a separate sloution and .Net Core
+
+HUlk-Angular -Soln
+WebApi---.NET CORE Soln
---
+++ WebApi/Controllers/McqController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class McqController : ControllerBase
+    {
+      
+
+        private readonly ILogger<McqController> _logger;
+
+        public McqController(ILogger<McqController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("getmcq")]
+        public IActionResult getdata()
+        {
+            HttpClient http = new HttpClient();
+            var result = http.GetAsync("https://opentdb.com/api.php?amount=5&category=11&difficulty=medium&type=multiple").Result.Content.ReadAsStringAsync().Result;
+
+            return Ok(result);
+        }
+    }
+}
